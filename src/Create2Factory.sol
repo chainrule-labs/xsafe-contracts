8361e86061600b684df83970698ecab0f87128d6
Adam Cuculich <46691282+cucupac@users.noreply.github.com>
2023-10-08 00:11:16 -0500

    Added ECDSA, revised domain separator, added utils and libraries folders.
    Need to unit test and empirically test on multiple chains to verify address similarity.

--- src/Create2Factory.sol
+++ src/Create2Factory.sol
@@ -1,70 +1,74 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.21;
 
+import {ECDSA} from "./utils/ECDSA.sol";
 
 contract Create2Factory {
-    event Deploy(address indexed sender, address indexed child, uint256 nonce);
+    // keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+    bytes32 constant DOMAIN_SEPARATOR_TYPEHASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
 
-    bytes32 constant DOMAIN_SEPARATOR_TYPEHASH = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
-    
+    bytes32 internal DOMAIN_SEPARATOR;
+
     // Mapping to keep track of user nonces
     mapping(address => uint256) public userNonces;
 
+    // Events
+    event Deploy(address indexed sender, address indexed child, bytes32 hashedBytecode, uint256 nonce);
+
+    constructor() {
+        DOMAIN_SEPARATOR = _computeDomainSeparator();
+    }
+
     // Computes and returns domain separator hash
-    function domainSeparator() public view returns (bytes32) {
-        return keccak256(abi.encode(
-            DOMAIN_SEPARATOR_TYPEHASH,
-            keccak256("Create2Factory"),
-            keccak256("1"),
-            block.chainid,
-            address(this)
-        ));
+    function domain_separator() public view returns (bytes32) {
+        if (DOMAIN_SEPARATOR != 0) {
+            return DOMAIN_SEPARATOR;
+        } else {
+            return _computeDomainSeparator();
+        }
+    }
+
+    function _computeDomainSeparator() internal view returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                DOMAIN_SEPARATOR_TYPEHASH, keccak256("Create2Factory"), keccak256("1"), block.chainid, address(this)
+            )
+        );
     }
 
     // Computes the hash to be signed off-chain
     function getTransactionHash(uint256 _nonce) public view returns (bytes32) {
-        bytes32 domainHash = domainSeparator();
-        return keccak256(abi.encode(domainHash, _nonce));
+        return keccak256(abi.encodePacked(DOMAIN_SEPARATOR, _nonce));
     }
 
     // Returns address to be created before actual deployment
-    function getAddress(bytes memory signature, bytes memory bytecode) public view returns (address) {
-        address userAddress = _recoverSigner(getTransactionHash(userNonces[msg.sender]), signature);
-        uint256 salt = uint256(bytes32(uint256(userAddress))) + userNonces[msg.sender];
-        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, keccak256(bytecode)));
+    function getAddress(bytes32 _hashedMessage, bytes memory _signature, bytes memory _bytecode)
+        public
+        view
+        returns (address)
+    {
+        address userAddress = ECDSA.recover(_hashedMessage, _signature);
+        uint256 salt = uint256(uint160(userAddress)) + userNonces[msg.sender];
+        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, keccak256(_bytecode)));
         return address(uint160(uint256(hash)));
     }
 
     // Deploys a new contract
-    function deploy(bytes memory signature, bytes memory bytecode) public returns (address) {
-        address userAddress = _recoverSigner(getTransactionHash(userNonces[msg.sender]), signature);
-        uint256 salt = uint256(bytes32(uint256(userAddress))) + userNonces[msg.sender];
-        address child;
+    function deploy(bytes32 _hashedMessage, bytes memory _signature, bytes memory _bytecode)
+        public
+        returns (address child)
+    {
+        address userAddress = ECDSA.recover(_hashedMessage, _signature);
+        uint256 currectNonce = userNonces[msg.sender];
 
+        uint256 salt = uint256(uint160(userAddress)) + currectNonce;
+
+        userNonces[msg.sender]++;
         assembly {
-            child := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
+            child := create2(callvalue(), add(_bytecode, 0x20), mload(_bytecode), salt)
             if iszero(extcodesize(child)) { revert(0, 0) }
         }
 
-        userNonces[msg.sender]++;
-        emit Deploy(msg.sender, child, userNonces[msg.sender]);
-        return child;
-    }
-
-    // Recover the signer address from a signature
-    function _recoverSigner(bytes32 _hash, bytes memory _sig) internal pure returns (address) {
-        (bytes32 r, bytes32 s, uint8 v) = _splitSignature(_sig);
-        return ecrecover(_hash, v, r, s);
-    }
-
-    // Splits a signature into (r, s, v)
-    function _splitSignature(bytes memory _sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
-        require(_sig.length == 65, "Invalid signature length");
-
-        assembly {
-            r := mload(add(_sig, 0x20))
-            s := mload(add(_sig, 0x40))
-            v := byte(0, mload(add(_sig, 0x60)))
-        }
+        emit Deploy(msg.sender, child, keccak256(_bytecode), currectNonce);
     }
 }
