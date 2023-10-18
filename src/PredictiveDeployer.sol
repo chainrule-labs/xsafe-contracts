f591448c55301bba0cacb00ef24e70d67569481b
Adam Cuculich <46691282+cucupac@users.noreply.github.com>
2023-10-17 21:47:41 -0500

    add security refactor logic
    * add extractNative and extractERC20
    * change signer to principal and remove requirement of a specific agent in deploy()
    
    ---------
    
    Co-authored-by: 0xernesto <78889960+0xernesto@users.noreply.github.com>

--- src/PredictiveDeployer.sol
+++ src/PredictiveDeployer.sol
@@ -2,26 +2,28 @@
 pragma solidity ^0.8.21;
 
 import {ECDSA} from "./dependencies/cryptography/ECDSA.sol";
+import {SafeTransferLib, ERC20} from "solmate/utils/SafeTransferLib.sol";
 import {Initializable} from "./dependencies/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "./dependencies/proxy/utils/UUPSUpgradeable.sol";
 import {Ownable} from "./dependencies/access/Ownable.sol";
-
-import "forge-std/console.sol";
+import {IERC20} from "./dependencies/token/interfaces/IERC20.sol";
 
 contract PredictiveDeployer is Initializable, UUPSUpgradeable, Ownable {
     // Private Constants: no SLOAD to save users gas
-    address private constant DEPLOYER = 0x76bd253e7a0FB5896b4ACA4b9ef06E9ee2b74e8E;
+    address private constant CONTRACT_DEPLOYER = 0x76bd253e7a0FB5896b4ACA4b9ef06E9ee2b74e8E; // TODO: Update
 
-    // Storage Variables
+    // EIP-712 Storage
     bytes32 internal DOMAIN_SEPARATOR;
+
+    // Factory Storage
     mapping(address => uint256) public userNonces;
 
     // Events
-    event Deploy(address indexed sender, address indexed child, bytes32 hashedBytecode, uint256 nonce);
+    event Deploy(address indexed principal, address indexed child, bytes32 hashedBytecode, uint256 nonce);
 
     /// @dev No constructor, so initialize Ownable explicitly.
     function initialize() public initializer {
-        require(msg.sender == DEPLOYER, "Invalid caller.");
+        require(msg.sender == CONTRACT_DEPLOYER, "Invalid caller.");
         DOMAIN_SEPARATOR = _computeDomainSeparator();
         __Ownable_init();
     }
@@ -34,58 +36,100 @@ contract PredictiveDeployer is Initializable, UUPSUpgradeable, Ownable {
             abi.encode(
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
                 keccak256("PredictiveDeployer"),
-                keccak256("1"), // Tracks upgrades
+                keccak256("1"), // Tracks contract upgrades
                 block.chainid,
                 address(this)
             )
         );
     }
 
-    // Computes the hash to be signed off-chain
-    function getTransactionHash(uint256 _nonce) public view returns (bytes32) {
-        return keccak256(abi.encodePacked(DOMAIN_SEPARATOR, _nonce));
+    /**
+     * @dev Computes the expected message hash.
+     * @param _principal The address of the account for whom this factory contract will deploy a child contract.
+     * @param _nonce The principal account's current nonce on this factory contract.
+     * @return messageHash The expected signed message hash.
+     */
+    function _computeMessageHash(address _principal, uint256 _nonce) internal view returns (bytes32) {
+        bytes32 txHash = getTransactionHash(_principal, _nonce);
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", txHash));
     }
 
-    // Returns address to be created before actual deployment
-    function getAddress(bytes32 _messageHash, bytes memory _signature, bytes memory _bytecode)
-        public
-        view
-        returns (address)
-    {
-        address signer = ECDSA.recover(_messageHash, _signature);
-        uint256 salt = uint256(uint160(signer)) + userNonces[signer];
+    /**
+     * @dev Returns the unique deployment transaction hash to be signed.
+     * @param _principal The address of the account for whom this factory contract will deploy a child contract.
+     * @param _nonce The principal account's current nonce on this factory contract.
+     * @return txHash The unique deployment transaction hash to be signed.
+     */
+    function getTransactionHash(address _principal, uint256 _nonce) public view returns (bytes32) {
+        return keccak256(abi.encodePacked(DOMAIN_SEPARATOR, _principal, _nonce));
+    }
+
+    /**
+     * @dev Returns the predicted address of a contract to be deployed before it's deployed.
+     * @param _principal The address of the account that signed the message hash.
+     * @param _bytecode The bytecode of the contract to be deployed.
+     * @return child The predicted address of the contract to be deployed.
+     */
+    function getAddress(address _principal, bytes memory _bytecode) public view returns (address) {
+        uint256 salt = uint256(uint160(_principal)) + userNonces[_principal];
         bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, keccak256(_bytecode)));
         return address(uint160(uint256(hash)));
     }
 
-    // Deploys a new contract
-    function deploy(bytes32 _messageHash, bytes memory _signature, bytes memory _bytecode) public returns (address) {
-        // recompute the hash
-        // What is being hashed?
+    /**
+     * @dev Deploys arbitrary child contracts at predictable addresses, derived from account signatures.
+     * @param _principal The address of the account that signed the message hash.
+     * @param _signature The resulting signature from the principal account signing the messahge hash.
+     * @param _bytecode The bytecode of the contract to be deployed.
+     * @return child The address of the deployed contract.
+     */
+    function deploy(address _principal, bytes memory _signature, bytes memory _bytecode) public returns (address) {
+        uint256 currectNonce = userNonces[_principal];
+        bytes32 expectedMessageHash = _computeMessageHash(_principal, currectNonce);
 
-        // address signer = ECDSA.recover(_messageHash, _signature);
-        // require(signer == msg.sender);
+        // Recover principal address
+        address recoveredSigner = ECDSA.recover(expectedMessageHash, _signature);
 
-        // uint256 currectNonce = userNonces[signer];
+        // Ensure the provided principal signed the expected message hash
+        require(recoveredSigner == _principal, "Unauthorized.");
 
-        // bytes32 txHash = keccak256(abi.encodePacked(DOMAIN_SEPARATOR, currectNonce));
-        // bytes32 messageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", txHash));
+        // Calculate salt
+        uint256 salt = uint256(uint160(_principal)) + currectNonce;
 
-        address signer = ECDSA.recover(_messageHash, _signature);
-        uint256 currectNonce = userNonces[signer];
+        // Update nonce state
+        userNonces[_principal]++;
 
-        uint256 salt = uint256(uint160(signer)) + currectNonce;
-
-        userNonces[signer]++;
-
+        // Deploy contract
         address child;
-
         assembly {
             child := create2(callvalue(), add(_bytecode, 0x20), mload(_bytecode), salt)
             if iszero(extcodesize(child)) { revert(0, 0) }
         }
 
-        emit Deploy(msg.sender, child, keccak256(_bytecode), currectNonce);
+        emit Deploy(_principal, child, keccak256(_bytecode), currectNonce);
         return child;
+    }
+
+    /* ****************************************************************************
+    **
+    **  Admin Functions
+    **
+    ******************************************************************************/
+
+    /**
+     * @dev Allow contract admin to extract native token.
+     */
+    function extractNative() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    /**
+     * @dev Allow contract admins to extract any ERC20 token.
+     * @param _token The address of token to remove.
+     */
+    function extractERC20(address _token) public onlyOwner {
+        uint256 balance = IERC20(_token).balanceOf(address(this));
+
+        SafeTransferLib.safeTransfer(ERC20(_token), msg.sender, balance);
     }
 }
