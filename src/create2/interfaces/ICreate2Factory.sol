c41b0563af3caa10e9007e2c2c8a03164912c7b0
0xernesto <78889960+0xernesto@users.noreply.github.com>
2023-11-01 22:07:01 -0500

    style: delete script directory and reorder functions in ICreate2Factory

--- src/create2/interfaces/ICreate2Factory.sol
+++ src/create2/interfaces/ICreate2Factory.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.21;
 
 interface ICreate2Factory {
     function userNonces(address _principal, bytes32 _hashedBytecode) external returns (uint256);
-    function getDeploymentHistory(address _principal) external returns (address[] memory);
     function getTransactionHash(address _principal, bytes memory _bytecode, uint256 _nonce)
         external
         returns (bytes32);
-    function getBytecodeHash(bytes memory _bytecode) external pure returns (bytes32);
     function getAddress(address _principal, bytes memory _bytecode) external returns (address);
+    function getBytecodeHash(bytes memory _bytecode) external pure returns (bytes32);
+    function getDeploymentHistory(address _principal) external returns (address[] memory);
     function deploy(address _principal, bytes memory _signature, bytes memory _bytecode) external returns (address);
 }
